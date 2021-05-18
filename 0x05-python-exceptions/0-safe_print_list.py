e9011ddf87f187e0dc17c912cb7dd3d1814ed9dc
Sebastián Paruma <jsebastian.paruma@gmail.com>
2021-05-18 00:03:31 +0000

    Fixing PEP8 style

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
 
+
 def safe_print_list(my_list=[], x=0):
 
-    printed = 0
-
-    try:
-        for i in range(0, x):
+    ret = 0
+    for i in range(x):
+        try:
             print("{}".format(my_list[i]), end="")
-            printed += 1
-        print("")
-    except IndexError:
-        print("")
-    return printed
+            ret += 1
+        except IndexError:
+            break
+    print("")
+    return (ret)
